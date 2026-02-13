3b34c90d6c86444dba2210429ac49e6b8071e889
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2026-02-13 16:02:08 +0400

    Create app.jsx
    creating app.jsx

---
+++ app.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import React, { useState, Suspense } from "react";
+import FormSubmit from "./FormSubmit";
+
+function App() {
+  return <FormSubmit></FormSubmit>;
+}
+
+export default App;
